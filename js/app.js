428f7c50cd8ad6224aa1bd15fc9f5117895a4478
Hanan Mufti <hananmufti@Hanans-MacBook-Pro.local>
2017-05-01 00:34:33 +0200

    Beautify code

--- js/app.js
+++ js/app.js
@@ -5,45 +5,44 @@
 
 // Enemies our player must avoid
 var surprise = 'images/surprise.jpg';
-var Enemy = function(x, y) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+var Enemy = function (x, y) {
+	// Variables applied to each of our instances go here,
+	// we've provided one for you to get started
+	// The image/sprite for our enemies, this uses
+	// a helper we've provided to easily load images
+	this.sprite = 'images/enemy-bug.png';
 	this.x = x;
 	this.y = y;
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-    //This crash function is when the player is crashing with the enemy then the player goes back to the start.
-	this.crash = function(){ 
-		if (((this.y > player.y - 30) && 
-			(this.y < player.y + 30))&&
-			((this.x > player.x - 50) && 
-			(this.x < player.x + 30))){
+Enemy.prototype.update = function (dt) {
+	// You should multiply any movement by the dt parameter
+	// which will ensure the game runs at the same speed for
+	// all computers.
+	//This crash function is when the player is crashing with the enemy then the player goes back to the start.
+	this.crash = function () {
+		if (((this.y > player.y - 30) &&
+			(this.y < player.y + 30)) &&
+			((this.x > player.x - 50) &&
+			(this.x < player.x + 30))) {
 				player.x = 300;
 				player.y = 489;
 		}
 	};
 	//This is the space between every row of the bugs.
-	if (this.x < 606){
-		this.x = (this.x + dt * Math.floor(Math.random()* 600)); 
+	if (this.x < 606) {
+		this.x = (this.x + dt * Math.floor(Math.random() * 600));
 		this.crash();
-    } 
-    else {
+	} else {
 		this.x = Math.floor(Math.random() * (-400)) - 300;
 	}
 };
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+Enemy.prototype.render = function () {
+	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Now write your own player class
@@ -54,84 +53,81 @@ Enemy.prototype.render = function() {
 // Place the player object in a variable called player
 var allEnemies = [];
 var spawn;
-for (spawn = 0; spawn < 4; spawn++) { 
-   		allEnemies[spawn] = new Enemy(Math.floor(Math.random()), ((spawn+1)*55)+(spawn*30));
+
+for (spawn = 0; spawn < 4; spawn++) {
+	allEnemies[spawn] = new Enemy(Math.floor(Math.random()), ((spawn + 1) * 55) + (spawn * 30));
 };
 
 // This is our kitten girl player.
-var Player = function() {
-    this.sprite = 'images/char-cat-girl.png';
-    this.x = 200;
-    this.y = 489;
-    this.score = 0;
+var Player = function () {
+	this.sprite = 'images/char-cat-girl.png';
+	this.x = 200;
+	this.y = 489;
+	this.score = 0;
 };
 
 var player = new Player();
 //This is the surprise after getting 50 points
-Player.prototype.update = function(dt){
+Player.prototype.update = function (dt) {
 
 	//Whenever the player reaches water, you'll get score
-	if (this.y <= 60) { 
-        this.score += 1;
-        player.x = 200;
+	if (this.y <= 60) {
+		this.score += 1;
+		player.x = 200;
 		player.y = 489;
 	}
 };
 
 //To render the Counter for the score and to render the player
-Player.prototype.render = function() {
-    	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-		ctx.font = 'bold 25px Arial';
-    	ctx.fillText('SCORE: ' + this.score, 460, 90);
-    	//When the score reaches 10, you'll see a picture!
-    	if (this.score === 10){
-			ctx.drawImage(Resources.get(surprise), 0, 120);
-			ctx.font = 'bold 75px Impact';
-			ctx.fillStyle = 'white';
-			ctx.fillText('GAME OVER!', 120, 190);
-			ctx.fillText('BE A FREAK!', 120, 600);
-			ctx.strokeText('GAME OVER!', 120, 190);
-			ctx.strokeText('BE A FREAK!', 120, 600);
-	 	} 
+Player.prototype.render = function () {
+	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+	ctx.font = 'bold 25px Arial';
+	ctx.fillText('SCORE: ' + this.score, 460, 90);
+	//When the score reaches 10, you'll see a picture!
+	if (this.score === 10) {
+		ctx.drawImage(Resources.get(surprise), 0, 120);
+		ctx.font = 'bold 75px Impact';
+		ctx.fillStyle = 'white';
+		ctx.fillText('GAME OVER!', 120, 190);
+		ctx.fillText('BE A FREAK!', 120, 600);
+		ctx.strokeText('GAME OVER!', 120, 190);
+		ctx.strokeText('BE A FREAK!', 120, 600);
+	}
 };
 //Borders here in game!! Do not cross!!
-Player.prototype.handleInput = function(direction){
-	if (((player.y < 60) && (direction === "up"))||
-		((player.y > 450) && (direction === "down"))||
-		((player.x < 0) && (direction === "left"))||
-		((player.x > 450) && (direction === "right")))
-	{
+Player.prototype.handleInput = function (direction) {
+	if (((player.y < 60) && (direction === "up")) ||
+		((player.y > 450) && (direction === "down")) ||
+		((player.x < 0) && (direction === "left")) ||
+		((player.x > 450) && (direction === "right"))) {
 		return;
 	}
-	
-//With if statements you can move the player!	
-		if (direction === "up"){
-			player.y = player.y - 83;
-			}
-		else if (direction === "down"){
-			player.y = player.y + 83;	
-			}			
-		else if (direction === "right"){
-			player.x = player.x + 101;	
-			}
-		else if (direction === "left"){
-			player.x = player.x - 101;	
-			}
+
+	//With if statements you can move the player!	
+	if (direction === "up") {
+		player.y = player.y - 83;
+	} else if (direction === "down") {
+		player.y = player.y + 83;
+	} else if (direction === "right") {
+		player.x = player.x + 101;
+	} else if (direction === "left") {
+		player.x = player.x - 101;
+	}
 };
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        65: 'left',
-        38: 'up',
-        87: 'up',
-        39: 'right',
-        68: 'right',
-        40: 'down',
-        83: 'down',
-    };
+document.addEventListener('keyup', function (e) {
+	var allowedKeys = {
+		37: 'left',
+		65: 'left',
+		38: 'up',
+		87: 'up',
+		39: 'right',
+		68: 'right',
+		40: 'down',
+		83: 'down',
+	};
 
-    player.handleInput(allowedKeys[e.keyCode]);
+	player.handleInput(allowedKeys[e.keyCode]);
 });
--- js/engine.js
+++ js/engine.js
@@ -14,7 +14,7 @@
  * a little simpler to work with.
  */
 
-var Engine = (function(global) {
+var Engine = (function (global) {
     /* Predefine the variables we'll be using within this scope,
      * create the canvas element, grab the 2D context for that canvas
      * set the canvas elements height/width and add it to the DOM.
@@ -47,7 +47,7 @@ var Engine = (function(global) {
          */
         update(dt);
         render();
-		
+
         /* Set our lastTime variable which is used to determine the time delta
          * for the next time this function is called.
          */
@@ -91,7 +91,7 @@ var Engine = (function(global) {
      * render methods.
      */
     function updateEntities(dt) {
-        allEnemies.forEach(function(enemy) {
+        allEnemies.forEach(function (enemy) {
             enemy.update(dt);
         });
         player.update();
@@ -108,13 +108,13 @@ var Engine = (function(global) {
          * for that particular row of the game level.
          */
         var rowImages = [
-                'images/water-block.png',   // Top row is water
-                'images/stone-block.png',   // Row 1 of 3 of stone
-                'images/stone-block.png',   // Row 2 of 3 of stone
-                'images/stone-block.png',   // Row 3 of 4 of stone
-				'images/stone-block.png',   // Row 4 of 4 of stone
-                'images/grass-block.png',   // Row 1 of 2 of grass
-                'images/grass-block.png'    // Row 2 of 2 of grass
+                'images/water-block.png', // Top row is water
+                'images/stone-block.png', // Row 1 of 3 of stone
+                'images/stone-block.png', // Row 2 of 3 of stone
+                'images/stone-block.png', // Row 3 of 4 of stone
+                'images/stone-block.png', // Row 4 of 4 of stone
+                'images/grass-block.png', // Row 1 of 2 of grass
+                'images/grass-block.png' // Row 2 of 2 of grass
             ],
             numRows = 7,
             numCols = 6,
@@ -149,7 +149,7 @@ var Engine = (function(global) {
         /* Loop through all of the objects within the allEnemies array and call
          * the render function you have defined.
          */
-        allEnemies.forEach(function(enemy) {
+        allEnemies.forEach(function (enemy) {
             enemy.render();
         });
 
@@ -173,9 +173,9 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-		'images/char-cat-girl.png',
-		'images/surprise.jpg',
-		'images/Heart.png',
+        'images/char-cat-girl.png',
+        'images/surprise.jpg',
+        'images/Heart.png',
     ]);
     Resources.onReady(init);
 
